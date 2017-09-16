c8dfe46ce372e37fefcee8faa89b85d387db0ee3
OmarNizam <eng.omarnizam@gmail.com>
2017-09-16 15:59:32 +0200

    wrapping out request in getProfile function and get profiles for many users

--- Complete Profile App/app.js
+++ Complete Profile App/app.js
@@ -1,6 +1,5 @@
 const https = require('https');
 // Function to print message to the console
-const username = "omarnizamaldeen";
 
 function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} poimts in JavaScript`;
@@ -8,24 +7,32 @@ function printMessage(username, badgeCount, points) {
 }
 // printMessage("Omar Nizam Aldeen", 100, 2000000);
 
+function getProfile(username) {
 // Connect to the api url ('https://teamtreehouse.com/username.json')
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-  //console.dir(response);
-  console.log(response.statusCode); // if 200 it's mean that the web request was successful and we able to connect to the API url
-  // Read the data
-  let body = "";
-  response.on('data', data => {
-    body += data.toString();
+  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    //console.dir(response);
+    console.log(response.statusCode); // if 200 it's mean that the web request was successful and we able to connect to the API url
+    // Read the data
+    let body = "";
+    response.on('data', data => {
+      body += data.toString();
+    });
+
+    // the end event
+    response.on('end', () => {
+      //Parse the data  : converting string to data structure
+      const profile = JSON.parse(body);
+      printMessage(username, profile.badges.length, profile.points.JavaScript);
+    });
+
+
+    //Print the data
+
   });
-
-  // the end event
-  response.on('end', () => {
-    //Parse the data  : converting string to data structure
-    const profile = JSON.parse(body);
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
-  });
-
-
-  //Print the data
-
+}
+//getProfile("omarnizamaldeen");
+//getProfile("chalkers");
+const users = ["chalkers", "omarnizamaldeen"];
+users.forEach( username => {
+  getProfile(username);
 });
