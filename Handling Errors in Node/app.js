f1a77bfb7392ed7b1554ade504d03abfc5c59bf2
OmarNizam <eng.omarnizam@gmail.com>
2017-09-19 12:52:50 +0200

    Handeling error event

--- Handling Errors in Node/app.js
+++ Handling Errors in Node/app.js
@@ -6,32 +6,30 @@ function printMessage(username, badgeCount, points) {
   console.log(message);
 }
 // printMessage("Omar Nizam Aldeen", 100, 2000000);
+
 function getProfile(username) {
-  // Connect to the api url ('https://teamtreehouse.com/username.json')
-  try {
-    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-      //console.dir(response);
-      console.log(response.statusCode); // if 200 it's mean that the web request was successful and we able to connect to the API url
-      // Read the data
-      let body = "";
-      response.on('data', data => {
-        body += data.toString();
-      });
-
-      // the end event
-      response.on('end', () => {
-        //Parse the data  : converting string to data structure
-        const profile = JSON.parse(body);
-        printMessage(username, profile.badges.length, profile.points.JavaScript);
-      });
-
+// Connect to the api url ('https://teamtreehouse.com/username.json')
+  const request = https.get(`https://wwwteamtreehouse.com/${username}.json`, response => {
+    //console.dir(response);
+    console.log(response.statusCode); // if 200 it's mean that the web request was successful and we able to connect to the API url
+    let body = "";
+        // Read the data
+    response.on('data', data => {
+      body += data.toString();
     });
 
-    request.on('error', error => console.error(`Problem with request: ${error.message}`));
-    } catch(error) {
-      console.log(error.message);
-    }
+    // the end event
+    response.on('end', () => {
+      //Parse the data  : converting string to data structure
+      const profile = JSON.parse(body);
+      printMessage(username, profile.badges.length, profile.points.JavaScript);
+    });
 
+
+    //Print the data
+  });
+  // error hanler event
+  request.on('error', error => console.error(`Problem with request: ${error.message}`));
 }
 //getProfile("omarnizamaldeen");
 //getProfile("chalkers");
