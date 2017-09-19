c37bdd9055ab7ab83f4df5f0370e9dc8b2492bcc
OmarNizam <eng.omarnizam@gmail.com>
2017-09-19 14:52:57 +0200

    fix the url without to be the code without errors

--- Handling Errors in Node/app.js
+++ Handling Errors in Node/app.js
@@ -19,7 +19,7 @@ function printMessage(username, badgeCount, points) {
 function getProfile(username) {
   try {
     // Connect to the API URL (https://teamtreehouse.com/username.json)
-    const request = https.get(`https://wwwteamtreehouse.com/${username}.json`, response => {
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
                             if (response.statusCode === 200) {
                             let body = "";
                             // Read the data
